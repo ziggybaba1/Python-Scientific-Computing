a6edb5312c20c1387f186c4e42d7ad853b314ccb
Ziggybaba1 <seyiadejugbagbe@gmail.com>
2022-08-29 03:03:21 +0100

    changed user input for movie

--- try_and_catch_statement.py
+++ try_and_catch_statement.py
@@ -1,7 +1,11 @@
 movie_title = "Salsa dance"
 try:
-    movie_title = int(movie_title)
+    movie_title = input("Please add movie part: ")
+    #purposely wants to test for error
+    movie_title = int(movie_title)  
 except:
     movie_title = -1
-
-print("Movie title is", movie_title)
+if movie_title != -1:
+     print("Movie part is: ",movie_title)
+else:
+     print("Movie part is", "Input error, it should be a number")
